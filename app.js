8087091dc34c10b344a45c49185cb7ae3b61db7b
Huymemee <tfhuy147@gmail.com>
2023-02-22 09:19:40 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
-const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
-app.use("/api/contacts",contactsRouter);
+var bodyParser = require('body-parser')
+
+
+const app =  express();
+
+
 app.use(cors());
+// app.use(express.urlencoded());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.json({message: " Welcome to contact book application." });
-});
+app.use("/api/contacts", contactsRouter);
 
-
+//handle 404 response
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((error, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        message: err.message || "Internal server error",
     });
 });
-        
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application."});
+});
+
 module.exports = app;
